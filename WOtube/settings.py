48776390ffd76fe1f142afe54f9323a980d4310a
Hayato-N24 <0409shido@gmail.com>
2021-09-05 23:40:03 +0900

    commit to save

--- WOtube/settings.py
+++ WOtube/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+YOUTUBE_DATA_API_KEY = 'AIzaSyDDCv4QdYsATnU2TGHp-9LrRNDq2A8sEjE'
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,26 @@
+import requests
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 from django.views.generic import TemplateView
 
 # Create your views here.
-class IndexView(TemplateView):
-    template_name = 'myapp/index.html'
 
+
+def Index(request):
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    params = {
+        'part': 'snippet',
+        'q': 'learn python',
+        'key': settings.YOUTUBE_DATA_API_KEY,
+        'maxResults': 9,
+        'type':'video'
+    }
+
+    video_ids = []
+    r = requests.get(search_url, params=params)
+    results = r.json()['items']
+    for result in results:
+        print(result['id']['videoId'])
+
+    return render(request, 'myapp/index.html')
